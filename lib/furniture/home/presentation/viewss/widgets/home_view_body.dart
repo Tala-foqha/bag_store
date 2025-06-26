67824b0dffe0e83dc5aa11194749f249bea94d0a
tala <foqhat835@gmail.com>
2025-06-26 21:45:57 +0300

    allBrands and seeAll widgets done

--- lib/furniture/home/presentation/viewss/widgets/home_view_body.dart
+++ lib/furniture/home/presentation/viewss/widgets/home_view_body.dart
@@ -1,9 +1,9 @@
 // furniture/home/presentation/viewss/widgets/home_view_body.dart
-import 'package:bag_store_ecommerec/core/widgets/custom_text_field.dart';
+import 'package:bag_store_ecommerec/core/utils/app_styles.dart';
 import 'package:bag_store_ecommerec/furniture/home/presentation/viewss/widgets/home_view_header.dart';
+import 'package:bag_store_ecommerec/furniture/home/presentation/viewss/widgets/home_view_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -17,36 +17,23 @@ class HomeViewBody extends StatelessWidget {
         children: [
           HomeViewHeader(),
           SizedBox(height: 16,),
-          Row(
-            children: [
-              Expanded(
-                child: CustomTextField(hintText: 'Search',
-                prefixIcon: SvgPicture.asset('assets/images/search.svg',
-                fit: BoxFit.scaleDown,
-                ),
-                
-                
-                ),
-              ),
-              SizedBox(width: 16,),
-              Container(
-          height: 48,
-          width: 48,
-          decoration: BoxDecoration(
-           border: Border.all(
-            color: Color(0xffEDEDED)
-           ),
-            borderRadius: BorderRadius.circular(16),
-            color:Color(0xff6C8947)
-          ),
-          child: SvgPicture.asset('assets/images/filter 01.svg',fit: BoxFit.scaleDown,
-          height: 20,
-          width: 20,
-          ),
-        )
+          HomeViewSearch(),
+          SizedBox(height: 16,),
+          Row(children: [
+            Text('All Brands',
+            style: AppStyles.meduim20.copyWith(
+              color: Color(0xff070A03),
+              fontSize: 18
+            ),
+            ),
+            Spacer(),
+            Text('See All',
+            style: AppStyles.Light14.copyWith(
+              color: Color(0xff6C8947)
+            ),
+            )
+          ],)
 
-            ],
-          )
 
       
         ],
---
+++ lib/furniture/home/presentation/viewss/widgets/home_view_search.dart
@@ -0,0 +1,45 @@
+// furniture/home/presentation/viewss/widgets/home_view_search.dart
+
+import 'package:bag_store_ecommerec/core/widgets/custom_text_field.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeViewSearch extends StatelessWidget {
+  const HomeViewSearch({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: CustomTextField(hintText: 'Search',
+          prefixIcon: SvgPicture.asset('assets/images/search.svg',
+          fit: BoxFit.scaleDown,
+          ),
+          
+          
+          ),
+        ),
+        SizedBox(width: 16,),
+        Container(
+    height: 48,
+    width: 48,
+    decoration: BoxDecoration(
+     border: Border.all(
+      color: Color(0xffEDEDED)
+     ),
+      borderRadius: BorderRadius.circular(16),
+      color:Color(0xff6C8947)
+    ),
+    child: SvgPicture.asset('assets/images/filter 01.svg',fit: BoxFit.scaleDown,
+    height: 20,
+    width: 20,
+    ),
+            )
+    
+      ],
+    );
+  }
+}
