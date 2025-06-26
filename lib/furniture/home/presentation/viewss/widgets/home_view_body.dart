8492bc124fd6af23276a2be6d92dfd8b2f9cb468
tala <foqhat835@gmail.com>
2025-06-26 21:40:41 +0300

    home view search done

--- lib/furniture/home/presentation/viewss/widgets/home_view_body.dart
+++ lib/furniture/home/presentation/viewss/widgets/home_view_body.dart
@@ -1,9 +1,9 @@
 // furniture/home/presentation/viewss/widgets/home_view_body.dart
-import 'package:bag_store_ecommerec/core/utils/app_styles.dart';
 import 'package:bag_store_ecommerec/core/widgets/custom_text_field.dart';
+import 'package:bag_store_ecommerec/furniture/home/presentation/viewss/widgets/home_view_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -15,39 +15,39 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Column(
-           // mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Hello, Alexa 👋🏻',style: AppStyles.Light14.copyWith(
-                color: Color(0xff070A03)
-              ),),
-            
-          
-          SizedBox(height: 2,),
+          HomeViewHeader(),
+          SizedBox(height: 16,),
           Row(
             children: [
-
-              Text('Let’s find some bag for you',
-              style: AppStyles.meduim20,
-              ),
-             Spacer(),
-              Container(
-                height: 48,
-                width: 48,
-                decoration: BoxDecoration(
-                 border: Border.all(
-                  color: Color(0xffEDEDED)
-                 ),
-                  borderRadius: BorderRadius.circular(16),
-                  color: Colors.white
+              Expanded(
+                child: CustomTextField(hintText: 'Search',
+                prefixIcon: SvgPicture.asset('assets/images/search.svg',
+                fit: BoxFit.scaleDown,
                 ),
-                child: SvgPicture.asset('assets/images/notification.svg',fit: BoxFit.scaleDown,),
-              )
-            ],
+                
+                
+                ),
+              ),
+              SizedBox(width: 16,),
+              Container(
+          height: 48,
+          width: 48,
+          decoration: BoxDecoration(
+           border: Border.all(
+            color: Color(0xffEDEDED)
+           ),
+            borderRadius: BorderRadius.circular(16),
+            color:Color(0xff6C8947)
           ),
-        
-            ]),
+          child: SvgPicture.asset('assets/images/filter 01.svg',fit: BoxFit.scaleDown,
+          height: 20,
+          width: 20,
+          ),
+        )
+
+            ],
+          )
+
       
         ],
       ),
