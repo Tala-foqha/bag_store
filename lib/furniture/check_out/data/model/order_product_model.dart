f571c2a3e9b1a93975f6a399ae884e196252b4e6
tala <foqhat835@gmail.com>
2025-07-04 18:24:47 +0300

    order product model done

--- lib/furniture/check_out/data/model/order_product_model.dart
+++ lib/furniture/check_out/data/model/order_product_model.dart
@@ -1,9 +1,11 @@
 // furniture/check_out/data/model/order_product_model.dart
+import 'package:bag_store_ecommerec/furniture/home/domain/entites/cart_item_entity.dart';
+
 class OrderProductModel {
   final String name;
   final String code;
   final String imageUrl;
-  final String price;
+  final num price;
   final int quantity;
 
   OrderProductModel({
@@ -12,4 +14,15 @@ class OrderProductModel {
   required this.imageUrl,
   required this.price, 
   required this.quantity});
+
+  factory OrderProductModel.fromEntity(CartItemEntity cartItem){
+    return OrderProductModel(
+      name: cartItem.productsEntity.bagName,
+     code: cartItem.productsEntity.code, 
+     imageUrl: cartItem.productsEntity.imageUrl,
+      price: cartItem.productsEntity.price, 
+      quantity: cartItem.count);
+
+      
+  }
 }
